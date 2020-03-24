0c880ce77c3fc4e606805af80796e9c8a80a90a5
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-03-24 11:29:56 +0100

    adding preview and description to Works

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_095929) do
+ActiveRecord::Schema.define(version: 2020_03_24_102900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 2020_03_24_095929) do
   create_table "works", force: :cascade do |t|
     t.string "title"
     t.string "category"
-    t.string "description"
-    t.string "preview"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.string "preview"
   end
 
 end
