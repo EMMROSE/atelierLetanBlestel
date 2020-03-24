efcca0f4cd5f0a7e23872ea823144e6bdf042d5b
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-03-25 00:45:58 +0100

    index for works with PG search

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,11 @@
 class Work < ApplicationRecord
   has_one_attached :cover
   has_many_attached :previews
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_category,
+    against: [ :title, :category ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
