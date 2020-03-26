b1c2f83b9ac225b6aa55cf8b0fd2d82a875c7baf
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-03-26 21:02:36 +0100

    form to contact - needs improvement

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,10 @@
 class OrderMailer < ApplicationMailer
-  def new_order_email
+  def create_confirmation
     @order = params[:order]
 
-    mail(to: <GMAIL_ADDRESS>, subject: "You got a new order!")
+    mail(
+      to:       <GMAIL_ADDRESS>,
+      subject:  "You got a new order!"
+    )
   end
 end
